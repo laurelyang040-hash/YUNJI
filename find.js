4e18996c3414819def81168aa2087517ddba6fe5
laurelyang040-hash <laurelyang040@gmail.com>
2026-05-04 22:08:53 +0800

    Delete find.js

--- find.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const h = fs.readFileSync('C:/Users/35447/Downloads/Cloudy/index.html', 'utf8');
-const idx = h.indexOf('wc-chat-background-layer');
-if (idx > 0) {
-    console.log('wc-chat-background-layer at line:', h.substring(0, idx).split('\n').length);
-}
